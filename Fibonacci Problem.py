485973c0dd8f75e844645efeff5ca0afa3d48be0
Gustavo Jimenez <gajl92@hotmail.com>
2024-09-10 23:18:18 -0600

    message

--- Fibonacci Problem.py
+++ Fibonacci Problem.py
@@ -1,7 +1,7 @@
-#Start by defining the outline of the program, tomorrow continue.
+#Start by defining the outline of the program, tomorrow continue. Check if Git works edit.
 #Site: https://www.practicepython.org/exercise/2014/04/30/13-fibonacci.html
 
-def fibonacci():git
+def fibonacci():
     '''
     Write a program that asks the user how many Fibonnaci numbers to generate and then generates them. Take this opportunity to think about how you can use functions. 
     Make sure to ask the user to enter the number of numbers in the sequence to generate.
@@ -14,7 +14,7 @@ def user_input():
     '''
     Ask User for the input of number, returns a number
     '''
-    return 6
+    return 4
     
 n = user_input()
 print(fibonacci(),n)
